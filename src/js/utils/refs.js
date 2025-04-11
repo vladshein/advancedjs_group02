19b3106032064ca4aa3078b4b6e4b96696899d93
KL-ML <knataly0513@gmail.com>
2025-04-11 16:12:22 +0200

    Update refs.js

--- src/js/utils/refs.js
+++ src/js/utils/refs.js
@@ -4,6 +4,8 @@ const refs = {
   exercises: document.querySelector('.exercise-list'),
   searchForm: document.querySelector('.search-form'),
   qouteWrap: document.querySelector('.quote-wrap'),
+  filterBtnsList: document.querySelector('.filter-btns-list'),
+  filterBtns: document.querySelectorAll('.js-filter-btn'),
 };
 
 export { refs };
