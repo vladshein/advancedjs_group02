6887a6847cbffa5de80735485130ef8d6d7abb19
KL-ML <knataly0513@gmail.com>
2025-04-11 10:26:26 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 import './js/partials_js/header';
 import { refs } from './js/utils/refs';
 import { handleSearch } from './js/partials_js/form';
-
+import { handleRendelQuote } from './js/partials_js/qoute';
 console.log(refs);
 refs.searchForm.addEventListener('submit', handleSearch);
+handleRendelQuote();
 
