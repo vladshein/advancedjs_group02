0ec5f2ea86970f1387968aa302b03140075cd079
KL-ML <knataly0513@gmail.com>
2025-04-11 16:12:24 +0200

    Update filters.js

--- src/js/services/filters.js
+++ src/js/services/filters.js
@@ -1,3 +1,32 @@
-class FiltersService {}
+import axios from 'axios';
+import { API_URL } from '../utils/consts.js';
+
+class FiltersService {
+  static filterQuery = '';
+
+  // метод що повертає значення обраної категорії після натискання на кнопку
+  getFilterQuery() {
+    return FiltersService.filterQuery;
+  }
+  // метод що зберігає значення обраного фільтру  після натискання на кнопку
+  setFilterQuery(value) {
+    FiltersService.filterQuery = value;
+  }
+    
+    async fetchFilteredData(limit) {
+        const axiosOptions = {
+          params: {
+            filter: this.getFilterQuery(),
+            limit: limit,
+            page: 1,
+          },
+        };
+        const { filteredData } = await axios.get(
+          `${API_URL}/filters/`,
+          axiosOptions
+        );
+        return filteredData;
+    }
+}
 
 export { FiltersService };
